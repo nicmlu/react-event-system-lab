4dc280054972aef69a0d45540ed9b90d1d1a3da3
nicmlu <NicoleMLucien@gmail.com>
2021-06-02 01:04:21 -0400

    commit tree a99f31ba17dfc747590b4c070f56c9bc223ec415

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+import React, { Component } from "react";
+
+export default class EyesOnMe extends Component {
+  handleKeyPress = e => {
+    if (e.key) {
+      console.log("Entering password..."));
+      resize("+");
+    } 
+  };
+
+  render() {
+    return (
+      <input type="password" onKeyPress={this.handleKeyPress} />
+    );
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { Component } from "react";
+
+export default class Keypad extends Component {
+   handleKeyPress = e => {
+    if (e.key) {
+      console.log("Entering password..."));
+      resize("+");
+    } 
+  };
+
+  render() {
+    return (
+      <input type="password" onKeyPress={this.handleKeyPress} />
+    );
+  }
+}
